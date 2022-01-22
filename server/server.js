734ecc98044891f1d500f120eefdc7fe143aed4a
dreamlabo <toddlabo@gamail.com>
2022-01-21 18:00:55 -0700

    reconfig package.json

--- server/server.js
+++ server/server.js
@@ -27,20 +27,29 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
 
 
 require('./routes/userRoutes')(app);
 require('./routes/albumRoutes')(app);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'));
 
-    const path = require('path');
-    app.get('*',(req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
+//     const path = require('path');
+//     app.get('*',(req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     });
 
-}
+// }
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
